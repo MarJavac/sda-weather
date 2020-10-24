9986a329ec9fe84042631253d28ff16f671de116
Marcin <marcinramza@gmail.com>
2020-10-24 13:44:48 +0200

    commit1

--- src/main/java/com/sda/weather/application/LocationRepository.java
+++ src/main/java/com/sda/weather/application/LocationRepository.java
@@ -1,6 +1,8 @@
 package com.sda.weather.application;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.mapping.MetadataSource;
 
 import java.util.Collections;
@@ -12,10 +14,13 @@ public class LocationRepository {
 
     public LocationRepository() {
         // todo: create the SessionFactory
-//        SessionFactory sessionFactory = new MetadataSource(serviceRegistry)
-//                .buildMetadata()
-//                .buildSessionFactory();
-//        sessionFactory = null;
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build()
+        SessionFactory sessionFactory = new MetadataSource(serviceRegistry)
+                .buildMetadata()
+                .buildSessionFactory();
+        sessionFactory = null;
     }
 
     public Location saveNewLocation(Location location) {
